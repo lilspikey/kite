9ee1fe194863c0fcc9bb41629940c5ad8aa9e7fa
John Montgomery <john@littlespikeyland.com>
2009-07-26 09:41:26 +0100

    added kite and string.

--- KiteCanvas.java
+++ KiteCanvas.java
@@ -8,6 +8,7 @@ public class KiteCanvas extends JPanel {
     private Space2D space = new Space2D();
     
     private ArrayList<Body2D> kiteString = new ArrayList<Body2D>();
+    private ArrayList<Body2D> kite = new ArrayList<Body2D>();
     
     public KiteCanvas() {
         space.setGravity( new Vector2D(0, -10) );
@@ -17,13 +18,47 @@ public class KiteCanvas extends JPanel {
         space.add(prev);
         kiteString.add(prev);
         
-        for ( int i = 0; i < 100; i++ ) {
-            Body2D bi = new Body2D(prev.getPos().x + 3, prev.getPos().y + 3);
+        for ( int i = 0; i < 150; i++ ) {
+            Body2D bi = new Body2D(prev.getPos().x + 2, prev.getPos().y + 2);
             space.add(bi);
             space.add(new StickConstraint<Body2D, Vector2D>(bi, prev));
             prev = bi;
             kiteString.add(prev);
         }
+        
+        Body2D center = prev;
+        
+        Body2D c1 = new Body2D(prev.getPos().x, prev.getPos().y + 20);
+        Body2D c2 = new Body2D(prev.getPos().x + 30, prev.getPos().y);
+        Body2D c3 = new Body2D(prev.getPos().x, prev.getPos().y - 40);
+        Body2D c4 = new Body2D(prev.getPos().x - 30, prev.getPos().y);
+        
+        kite.add(c1);
+        kite.add(c2);
+        kite.add(c3);
+        kite.add(c4);
+        
+        space.add(c1);
+        space.add(c2);
+        space.add(c3);
+        space.add(c4);
+        
+        // constraints from center to corners
+        space.add(new StickConstraint<Body2D, Vector2D>(center, c1));
+        space.add(new StickConstraint<Body2D, Vector2D>(center, c2));
+        space.add(new StickConstraint<Body2D, Vector2D>(center, c3));
+        space.add(new StickConstraint<Body2D, Vector2D>(center, c4));
+        
+        // constraints between corners
+        space.add(new StickConstraint<Body2D, Vector2D>(c1, c2));
+        space.add(new StickConstraint<Body2D, Vector2D>(c2, c3));
+        space.add(new StickConstraint<Body2D, Vector2D>(c3, c4));
+        space.add(new StickConstraint<Body2D, Vector2D>(c4, c1));
+        // cross
+        space.add(new StickConstraint<Body2D, Vector2D>(c1, c3));
+        space.add(new StickConstraint<Body2D, Vector2D>(c2, c4));
+        
+        
     }
     
     public void tick() {
@@ -37,14 +72,34 @@ public class KiteCanvas extends JPanel {
         g.clearRect(0, 0, getWidth(), height);
         g.translate(0, -10);
         
+        g.setColor(Color.RED);
+        drawPolygon(g, kite, true);
+        
+        g.setColor(Color.BLACK);
+        drawLine(g, kite.get(0).getPos(), kite.get(2).getPos());
+        drawLine(g, kite.get(1).getPos(), kite.get(3).getPos());
+        
+        g.setColor(Color.GRAY);
+        drawPolygon(g, kiteString, false);
+    }
+    
+    private void drawPolygon(Graphics g, java.util.List<Body2D> bodies, boolean join) {
         Vector2D prevPos = null;
-        for ( Body2D b: kiteString ) {
+        if ( join && bodies.size() > 0 ) {
+            prevPos = bodies.get(bodies.size()-1).getPos();
+        }
+        for ( Body2D b: bodies ) {
             Vector2D pos = b.getPos();
             if ( prevPos != null ) {
-                g.drawLine((int)(pos.x), (int)(height - pos.y), (int)(prevPos.x), (int)(height - prevPos.y));
+                drawLine(g, pos, prevPos);
             }
             prevPos = pos;
         }
+    }
+    
+    private void drawLine(Graphics g, Vector2D p1, Vector2D p2) {
+        int height = getHeight();
+        g.drawLine((int)(p1.x), (int)(height - p1.y), (int)(p2.x), (int)(height - p2.y));
     }
     
     @Override
