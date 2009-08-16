c3e285b759f858b804bee472fe1fd363750b7e5a
John Montgomery <john@littlespikeyland.com>
2009-08-16 19:45:48 +0100

    work on getting some sort of basic drop-shadow working. not really fast enought and needs refactoring into separate class.

--- com/psychicorigami/scene/Layer.java
+++ com/psychicorigami/scene/Layer.java
@@ -3,15 +3,25 @@ package com.psychicorigami.scene;
 import java.awt.Graphics2D;
 import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.GraphicsEnvironment;
+import java.awt.GraphicsConfiguration;
+import java.awt.Transparency;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.awt.image.ConvolveOp;
 import java.awt.image.Kernel;
 import java.awt.image.BufferedImageOp;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.awt.image.*;
+
 public class Layer {
     private BufferedImage backing = null;
+    private int[] source = null;
+    private int[] destination = null;
     
     private List<Shape> shapes = new ArrayList<Shape>();
         
@@ -21,7 +31,11 @@ public class Layer {
     }
     
     public void setBacking(int width, int height) {
-        backing = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsConfiguration gc = ge.getDefaultScreenDevice().getDefaultConfiguration();
+        backing = gc.createCompatibleImage(width, height, Transparency.TRANSLUCENT);
+        source = new int[width*height];
+        destination = new int[width*height];
     }
     
     public void paint(Graphics2D g) {
@@ -30,6 +44,7 @@ public class Layer {
         }
         else {            
             Graphics2D gb = backing.createGraphics();
+            gb.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             
             // set to all transparent
             gb.setComposite(AlphaComposite.getInstance(AlphaComposite.CLEAR, 0.0f));
@@ -46,13 +61,55 @@ public class Layer {
             
             gb.dispose();
             
+            BufferedImage blurred = blur(backing);
+            
             // render shadow
-            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.2f));
-            g.drawImage(backing, 5, -5, null);
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+            g.drawImage(blurred, 1, -1, null);
             
             // draw original
             g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
             paintShapes(g);
+        }
+    }
+    
+    public BufferedImage blur(BufferedImage img) {
+        int width = img.getWidth(), height = img.getHeight();
+        
+        WritableRaster raster = img.getRaster();
+        raster.getDataElements(0, 0, width, height, source);
+        
+        convolveAndTranspose(source, destination, width, height);
+        convolveAndTranspose(destination, source, width, height);
+        
+        raster.setDataElements(0, 0, width, height, source);
+        
+        return img;
+    }
+    
+    public static void convolveAndTranspose(int[] inPixels, int[] outPixels, int width, int height) {
+        for (int y = 0; y < height; y++) {
+            int index = y;
+            int ioffset = y*width;
+            for (int x = 0; x < width; x++) {
+                int sum = 0;
+                int count = 0;
+                
+                int radius = 2;
+                
+                int xleft  = Math.max(0, x-radius);
+                int xright = Math.min(width-1, x+radius);
+                
+                for ( int i = xleft; i <= xright; i++ ) {
+                    int p = inPixels[ioffset + i];
+                    int a = (p >> 24) & 0xFF;
+                    sum += a;
+                    count++;
+                }
+                
+                outPixels[index] = ((sum/count) & 0xFF) << 24;
+                index += height;
+            }
         }
     }
     
