30172cc7a9c2e0f8540b5e2514015529d6496851
John Montgomery <john@littlespikeyland.com>
2009-11-08 12:37:25 +0000

    changed ColoriseLayerStyle to use Variables (so can dynamically control color etc).

--- src/main/java/com/psychicorigami/scene/ColoriseLayerStyle.java
+++ src/main/java/com/psychicorigami/scene/ColoriseLayerStyle.java
@@ -15,16 +15,35 @@ import java.awt.image.DataBufferInt;
 import java.util.Map;
 import java.util.LinkedHashMap;
 
+import com.psychicorigami.variable.Variable;
+import com.psychicorigami.variable.ConstVariable;
+
 public class ColoriseLayerStyle implements LayerStyle {
-    private float opacity = 0.5f;
-    private Color color = new Color(0x000000);
+    private Variable<Float> opacity = new ConstVariable<Float>(0.5f);
+    private Variable<Color> color   = new ConstVariable<Color>(new Color(0x000000));
     
     public void setOpacity(float opacity) {
+        setOpacity(new ConstVariable<Float>(opacity));
+    }
+    
+    public void setOpacity(Variable<Float> opacity) {
         this.opacity = opacity;
     }
     
     public float getOpacity() {
-        return opacity;
+        return opacity.val();
+    }
+    
+    public Color getColor() {
+        return color.val();
+    }
+    
+    public void setColor(Color color) {
+        setColor(new ConstVariable<Color>(color));
+    }
+    
+    public void setColor(Variable<Color> color) {
+        this.color = color;
     }
     
     public void shapeAdded(Shape shape) {
@@ -42,8 +61,8 @@ public class ColoriseLayerStyle implements LayerStyle {
     public void postRender(Layer layer, BufferedImage image) {
         Graphics2D g = image.createGraphics();
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, opacity));
-        g.setColor(color);
+        g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, getOpacity()));
+        g.setColor(getColor());
         g.fillRect(0, 0, image.getWidth(), image.getHeight());
         
         g.dispose();
